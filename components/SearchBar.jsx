31101c6af09b3354d797ab231ac9137e1aa2a468
mburuanthony <mburuantony@outlook.com>
2023-11-08 00:03:32 +0300

    minor fixes & updates

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { StyleSheet, View, TextInput, Pressable } from "react-native";
+import { StatusBar } from "expo-status-bar";
 import { EvilIcons } from "@expo/vector-icons";
 import { colors } from "../assets/style/colors";
 import { useAudioContext } from "../context/AudioProvider";
@@ -24,46 +25,52 @@ export const SearchBar = ({ showSearch, hideSearchBarFunc }) => {
   }, [text, showSearch]);
 
   return (
-    <View style={styles.container}>
-      <TextInput
-        autoFocus
-        placeholder="Search my music.."
-        placeholderTextColor={colors.white}
-        style={styles.textInput}
-        value={text}
-        onChangeText={(value) => setTxt(value)}
-        onChange={() => filterAudio()}
-      />
-      <Pressable
-        onPress={() => {
-          setTxt("");
-          hideSearchBarFunc();
-          getAudioFiles();
-        }}
-      >
-        <EvilIcons name="close" size={24} color={colors.white} />
-      </Pressable>
-    </View>
+    <>
+      <View style={styles.container}>
+        <TextInput
+          autoFocus
+          placeholder="Search my music.."
+          placeholderTextColor={colors.textfaint}
+          style={styles.textInput}
+          value={text}
+          onChangeText={(value) => setTxt(value)}
+          onKeyPress={() => filterAudio()}
+        />
+        <Pressable
+          onPress={() => {
+            setTxt("");
+            hideSearchBarFunc();
+            getAudioFiles();
+          }}
+        >
+          <EvilIcons name="close" size={18} color={colors.textfaint} />
+        </Pressable>
+      </View>
+      <StatusBar backgroundColor={colors.alertbg} style="light" />
+    </>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    width: "100%",
+    padding: 8,
+    paddingBottom: 10,
+    borderWidth: 0,
+    borderBottomWidth: 0.5,
+    borderBottomColor: colors.divider,
     position: "absolute",
-    top: 0,
+    top: 26,
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
-    marginTop: 40,
-    width: "100%",
-    padding: 8,
-    backgroundColor: "transparent",
+    backgroundColor: colors.alertbg,
     zIndex: 2000,
   },
   textInput: {
     width: "90%",
-    color: colors.white,
-    fontFamily: "mukta-regular",
-    fontSize: 16,
+    color: colors.textdefault,
+    fontSize: 12,
+    fontFamily: "ops-light",
   },
 });
