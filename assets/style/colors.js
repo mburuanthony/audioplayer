143ea387cfed5de3030d7198865b75def96a2e8f
mburuanthony <mburuantony@outlook.com>
2023-11-08 00:03:13 +0300

    colors

--- assets/style/colors.js
+++ assets/style/colors.js
@@ -1,4 +1,7 @@
 export const colors = {
-  primary: "#0F0E0E",
-  white: "#FFFFFF",
+  primary: "#262626",
+  alertbg: "#323232",
+  divider: "rgba(248, 250, 252, 0.2)",
+  textdefault: "#F8FAFC",
+  textfaint: "#7F7F7F",
 };
