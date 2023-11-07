ef138f9d55722e4bcf2a47112e59942b67a63e10
mburuanthony <mburuantony@outlook.com>
2023-11-08 00:03:26 +0300

    minor fixes & updates

--- components/AudioList.jsx
+++ components/AudioList.jsx
@@ -1,4 +1,4 @@
-import { FlatList, StyleSheet, View } from "react-native";
+import { FlatList, StyleSheet } from "react-native";
 import { useAudioContext } from "../context/AudioProvider";
 import { Audio } from "./Audio";
 
