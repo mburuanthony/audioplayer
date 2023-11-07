b5d34024b47f4867ca38f250318c93304d17092d
mburuanthony <mburuantony@outlook.com>
2023-11-08 00:03:29 +0300

    minor fixes & updates

--- components/BottomPlayer.jsx
+++ components/BottomPlayer.jsx
@@ -1,51 +1,107 @@
 import { useEffect, useState } from "react";
-import { Pressable, StyleSheet, Text, View } from "react-native";
-import { Feather } from "@expo/vector-icons";
-import { AntDesign } from "@expo/vector-icons";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Pressable, StyleSheet, View, Dimensions } from "react-native";
+import {
+  Ionicons,
+  SimpleLineIcons,
+  MaterialCommunityIcons,
+} from "@expo/vector-icons";
 import { useAudioContext } from "../context/AudioProvider";
 import { Audio } from "expo-av";
 import { colors } from "../assets/style/colors";
 
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
+
 export const BottomPlayer = () => {
-  const { audioFiles, selectedAudio, setSelecedAudio } = useAudioContext();
+  const { audioFiles, selectedAudio, updateSelectedAUdio } = useAudioContext();
   const [Sound, setSound] = useState();
-  const [currIndex, setCurrIndex] = useState(0);
-  const [isPlaying, setIsPlaying] = useState(false);
-
-  const playPauseAudio = async () => {
-    const { sound } = await Audio.Sound.createAsync({
-      uri: selectedAudio.URI,
-    });
-    setSound(sound);
-
-    await sound.playAsync();
-    setIsPlaying(true);
-  };
+  const [currpos, setcurrpos] = useState(0);
+  const [progress, setprogress] = useState(0);
+  const [isplaying, setisplaying] = useState(false);
 
   const playNext = () => {
-    setSelecedAudio({
-      title: audioFiles[currIndex]?.filename,
-      URI: audioFiles[currIndex]?.uri,
-    });
-
-    setCurrIndex((prevIndex) => (prevIndex += 1));
-    if (currIndex >= audioFiles.length - 1) setCurrIndex(0);
+    const currsongidx = audioFiles.findIndex(
+      (audiofile) => audiofile?.filename === selectedAudio?.title
+    );
+    if (currsongidx == audioFiles.length - 1) {
+      updateSelectedAUdio({
+        filename: audioFiles[0]?.filename,
+        URI: audioFiles[0]?.uri,
+      });
+    } else {
+      const nextidx = currsongidx + 1;
+      updateSelectedAUdio({
+        filename: audioFiles[nextidx]?.filename,
+        URI: audioFiles[nextidx]?.uri,
+      });
+    }
   };
 
   const playPrevious = () => {
-    setSelecedAudio({
-      title: audioFiles[currIndex]?.filename,
-      URI: audioFiles[currIndex]?.uri,
-    });
+    const currsongidx = audioFiles.findIndex(
+      (audiofile) => audiofile?.filename === selectedAudio?.title
+    );
 
-    if (currIndex <= 0) setCurrIndex(audioFiles.length);
-    setCurrIndex((prevIndex) => (prevIndex -= 1));
+    if (currsongidx == 0) {
+      updateSelectedAUdio({
+        filename: audioFiles[audioFiles.length - 1]?.filename,
+        URI: audioFiles[audioFiles.length - 1]?.uri,
+      });
+    } else {
+      const previdx = currsongidx - 1;
+
+      updateSelectedAUdio({
+        filename: audioFiles[previdx]?.filename,
+        URI: audioFiles[previdx]?.uri,
+      });
+    }
   };
 
-  useEffect(() => {
-    playPauseAudio();
-  }, [selectedAudio.URI]);
+  const rewind30 = () => {
+    Sound.setPositionAsync(currpos - 30 * 1000);
+  };
+
+  const forward30 = () => {
+    Sound.setPositionAsync(currpos + 30 * 1000);
+  };
+
+  const AudioPlaybackStatusUpdate = (playbackStatus) => {
+    if (playbackStatus.isPlaying) {
+      setisplaying(true);
+      setcurrpos(playbackStatus.positionMillis);
+
+      const progress =
+        (playbackStatus?.positionMillis / playbackStatus?.durationMillis) * 100;
+
+      setprogress(progress);
+    } else {
+      setisplaying(false);
+    }
+
+    if (playbackStatus.didJustFinish) {
+      setisplaying(false);
+      setcurrpos(0);
+      playNext();
+    }
+  };
+
+  const playaudio = async () => {
+    if (currpos !== 0 && Sound._loaded) {
+      Sound.playFromPositionAsync(currpos);
+    } else {
+      const { sound } = await Audio.Sound.createAsync(
+        { uri: selectedAudio.URI },
+        { shouldPlay: true, isLooping: false }
+      );
+
+      if (sound._loaded) {
+        setSound(sound);
+        sound.setOnPlaybackStatusUpdate(AudioPlaybackStatusUpdate);
+        await sound.playAsync();
+      }
+    }
+  };
+
+  const pauseaudio = () => Sound.pauseAsync();
 
   useEffect(() => {
     return Sound
@@ -53,85 +109,108 @@ export const BottomPlayer = () => {
           Sound.unloadAsync();
         }
       : undefined;
-  }, [Sound]);
+  }, [Sound, selectedAudio.URI]);
+
+  useEffect(() => {
+    playaudio();
+  }, [selectedAudio.URI]);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>
-        {selectedAudio.title.length >= 26
-          ? `${selectedAudio.title.substring(0, 26)}...`
-          : selectedAudio.title}
-      </Text>
-
       <View style={styles.actions}>
         <Pressable onPress={playPrevious}>
-          <MaterialCommunityIcons
-            name="skip-previous-outline"
-            size={28}
-            color={colors.white}
+          <Ionicons
+            name="play-skip-back-outline"
+            size={20}
+            color={colors.textdefault}
           />
         </Pressable>
 
-        {isPlaying === true ? (
-          <Pressable
-            style={styles.playPause}
-            onPress={async () => {
-              await Sound?.pauseAsync();
-              setIsPlaying(false);
-            }}
-          >
-            <AntDesign name="pause" size={30} color={colors.white} />
-          </Pressable>
-        ) : (
-          <Pressable
-            style={styles.playPause}
-            onPress={async () => {
-              await Sound?.playAsync();
-              setIsPlaying(true);
-            }}
-          >
-            <Feather name="play" size={30} color={colors.white} />
-          </Pressable>
-        )}
+        <Pressable style={styles.rewndfrwdicns} onPress={rewind30}>
+          <MaterialCommunityIcons
+            name="rewind-30"
+            size={18}
+            color={colors.textdefault}
+          />
+        </Pressable>
+
+        <Pressable
+          style={styles.playPause}
+          onPress={isplaying ? pauseaudio : playaudio}
+        >
+          {isplaying ? (
+            <SimpleLineIcons
+              name="control-pause"
+              size={18}
+              color={colors.textdefault}
+            />
+          ) : (
+            <SimpleLineIcons
+              name="control-play"
+              size={18}
+              color={colors.textdefault}
+            />
+          )}
+        </Pressable>
+
+        <Pressable style={styles.rewndfrwdicns} onPress={forward30}>
+          <MaterialCommunityIcons
+            name="fast-forward-30"
+            size={18}
+            color={colors.textdefault}
+          />
+        </Pressable>
 
         <Pressable onPress={playNext}>
-          <MaterialCommunityIcons
-            name="skip-next-outline"
-            size={28}
-            color={colors.white}
+          <Ionicons
+            name="play-skip-forward-outline"
+            size={20}
+            color={colors.textdefault}
           />
         </Pressable>
       </View>
+
+      <View
+        style={[
+          styles.progressbar,
+          {
+            width: `${progress}%`,
+          },
+        ]}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
+    width: SCREEN_WIDTH,
+    height: 64,
     position: "absolute",
     bottom: 0,
-    width: "100%",
-    height: 70,
-    padding: 10,
-    borderTopLeftRadius: 6,
-    borderTopRightRadius: 6,
-    backgroundColor: "rgba(2,2,22,0.35)",
-  },
-  title: {
-    marginLeft: 8,
-    color: colors.white,
-    fontFamily: "mukta-regular",
-    fontSize: 18,
-  },
-  playPause: {
-    marginHorizontal: 10,
+    justifyContent: "center",
+    paddingHorizontal: 8,
+    borderWidth: 0,
+    borderTopWidth: 0.5,
+    borderColor: colors.divider,
+    backgroundColor: colors.alertbg,
   },
   actions: {
+    width: "100%",
     flexDirection: "row",
     alignItems: "center",
-    // marginRight: 4,
+    justifyContent: "center",
+  },
+  playPause: {
+    marginHorizontal: 18,
+  },
+  rewndfrwdicns: {
+    marginHorizontal: 24,
+  },
+  progressbar: {
+    height: 2,
+    marginTop: 12,
+    borderRadius: 8,
+    backgroundColor: colors.divider,
   },
 });
