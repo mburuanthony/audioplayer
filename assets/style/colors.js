45e5fe9ba92fe81fb6917f3334952c8aa342dbb9
mburuanthony <mburuantony@outlook.com>
2023-11-27 01:13:31 +0300

    minor updates

--- assets/style/colors.js
+++ assets/style/colors.js
@@ -2,7 +2,7 @@ export const colors = {
   primary: "#1F1F1F",
   secondary: "#262626",
   dividerfaint: "rgba(248, 250, 252, 0.1)",
-  dividerhighlight: "rgba(248, 250, 252, 0.1)",
+  dividerhighlight: "rgba(248, 250, 252, 0.45)",
   textdefault: "#F8FAFC",
   textfaint: "#7F7F7F",
 };
