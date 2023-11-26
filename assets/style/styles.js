b18372fe8ce590cf9ffced27562c396b8d4d87d2
mburuanthony <mburuantony@outlook.com>
2023-11-26 21:47:48 +0300

    reusable styles

---
+++ assets/style/styles.js
@@ -0,0 +1,13 @@
+import { colors } from "./colors";
+
+export const text = {
+  fontSize: 12,
+  fontFamily: "ops-light",
+  color: colors.textfaint,
+};
+
+export const title = {
+  fontSize: 16,
+  fontFamily: "ops-regular",
+  color: colors.textfaint,
+};
