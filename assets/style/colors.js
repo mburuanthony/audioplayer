b8c7dd45321f1bfcfedb6f4f00e33818e0eb3caf
mburuanthony <mburuantony@outlook.com>
2023-11-28 15:44:18 +0300

    added color

--- assets/style/colors.js
+++ assets/style/colors.js
@@ -1,6 +1,7 @@
 export const colors = {
   primary: "#1F1F1F",
   secondary: "#262626",
+  accent: "rgba(248, 250, 252, 0.05)",
   dividerfaint: "rgba(248, 250, 252, 0.1)",
   dividerhighlight: "rgba(248, 250, 252, 0.45)",
   textdefault: "#F8FAFC",
