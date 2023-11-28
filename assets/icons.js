4b11127ce3d53e7fd729fa37ac3bf3c969fc9cde
mburuanthony <mburuantony@outlook.com>
2023-11-28 15:55:13 +0300

    updated pauseicon

--- assets/icons.js
+++ assets/icons.js
@@ -53,8 +53,8 @@ export const NextIcon = () => {
 export const PauseIcon = () => {
   return (
     <Svg
-      width="22"
-      height="22"
+      width="24"
+      height="24"
       viewBox="0 0 22 22"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
